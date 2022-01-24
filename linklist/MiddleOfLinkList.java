5ace59544760a30bdad21902e830231b59bfb04e
atikalkar <atikalkar@paypal.com>
2022-01-24 13:36:46 +0530

    add folder to java sourcepath

--- linklist/MiddleOfLinkList.java
+++ linklist/MiddleOfLinkList.java
@@ -1,32 +1,44 @@
 
 public class MiddleOfLinkList {
-    public int getLen(ListNode head){
-        int n =0;
-        while(head != null){
+    public int getLen(ListNode head) {
+        int n = 0;
+        while (head != null) {
             head = head.next;
             n++;
         }
         return n;
     }
+re
     public ListNode middleNode(ListNode head) {
         int n = getLen(head);
-        int m = n/2;
-        while(m !=0){
+        int m = n / 2;
+        while (m != 0) {
             head = head.next;
             m--;
         }
         return head;
     }
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         /**
-         * Given the head of a singly linked list, return the middle node of the linked list.
+         * Given the head of a singly linked list, return the middle node of the linked
+         * list.
          */
     }
 }
+
 class ListNode {
     int val;
-   ListNode next;
-   ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
